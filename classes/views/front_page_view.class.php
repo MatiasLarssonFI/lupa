2b0565ad61cd3f546631e07e163bea2f6335149b
Matias Larsson <matias.larsson@gmail.com>
2017-01-23 21:19:00 +0200

    Layout improvements and the 'Services' title

--- classes/views/front_page_view.class.php
+++ classes/views/front_page_view.class.php
@@ -25,6 +25,7 @@ class FrontPageView extends AbstractView {
         return array(
             "strings" => array(
                 "page_title" => $text_storage->text("FRONT_PAGE_TITLE"),
+                "services_title" => $text_storage->text("FRONT_PAGE_SERVICES_TITLE"),
                 "slideshow_caption_title" => $text_storage->text("FRONT_PAGE_SH_CAPTION_TITLE"),
                 "slideshow_caption_paragraph" => $text_storage->text("FRONT_PAGE_SH_CAPTION_TEXT"),
             ),
