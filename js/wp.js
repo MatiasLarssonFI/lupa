cc6ebb60925456ee1449f0ed723babfd60ec2c64
MatiasLarssonFI <matias.larsson@gmail.com>
2018-02-08 21:25:41 +0200

    Mobile nav layout and bg. Logo size.

--- js/wp.js
+++ js/wp.js
@@ -1,1 +1,43 @@
-var wp = {};
+var wp = {}
+
+var lupa = {
+    onLoad: function () {
+        var toggleNav = function() {
+            var btn = $(this);
+            var node = $(btn.data("target"));
+            var btn_warp = btn.closest(".navbar-toggle-img-btn-container");
+            var w = btn_warp.outerWidth();
+            var h = btn_warp.height();
+            var nav_w = 0;
+            node.find("[data-link-text]").each(function() {
+                var len = $(this).text().length
+                if (len > nav_w) {
+                    nav_w = len;
+                }
+            });
+            node.css({
+                position: "absolute",
+                top: btn_warp.offset().top + h + "px",
+                right: "15px",
+                zIndex: 9999,
+                minWidth: w,
+                width: nav_w / 1.5 + "em"
+            });
+            node.toggleClass("hidden");
+        };
+        flyout_toggle = $("[data-toggle='flyout']");
+        flyout_toggle.on("click", toggleNav);
+        flyout_toggle.on("blur", function () {
+            var btn = $(this);
+            var node = $(btn.data("target"));
+            setTimeout(function () {
+                node.addClass("hidden");
+            }, 100);
+        });
+    }
+};
+
+
+$(document).ready(function() {
+    lupa.onLoad();
+});
