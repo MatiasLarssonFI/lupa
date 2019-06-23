ff03c0f5233eb371a0436be7ffc2889491927a3b
MatiasLarssonFI <matias.larsson@gmail.com>
2019-06-23 11:19:25 +0200

    error_log

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
 ini_set("error_log", __DIR__ . "/error_log");
-ini_set("display_errors", 0);
+ini_set("log_errors", "1");
+ini_set("display_errors", "0");
 ini_set("error_reporting", E_ALL | E_STRICT);
 
 
