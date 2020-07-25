000ebf665d12cb0c30cd0a2194c2d59824b58e8a
MatiasLarssonFI <matias.larsson@gmail.com>
2020-07-25 12:36:26 +0200

    Update item counts on submit
    Also, improve JS code structure a bit.

--- js/work_list.js
+++ js/work_list.js
@@ -1,16 +1,14 @@
 lupa.workList = {
+    // TBD: notes separate save
     init : function(baseUrl) {
+        var self = this;
+        
         $("[data-item-action]").on("click", function() {
             var $btn = $(this)
             var action = $btn.data("item-action").toLowerCase();
             var spinnerClass = $.inArray(action, [ "archive", "halt", "notes" ]) !== -1 ? action : "action";
             var $item = $btn.closest("[data-list-item]");
             var $spinner = $item.find("[data-item-" + spinnerClass + "-spinner]");
-            
-            var onErr = function() {
-                alert("Sorry, but an error has occured. Please try again or refresh the page.");
-                $btn.prop("disabled", false);
-            };
             
             $.ajax({
                 type: "post",
@@ -29,21 +27,42 @@ lupa.workList = {
                 },
                 success : function(response) {
                     if (response.is_success) {
-                        $btn.text("DONE");
-                        $item.delay(250).fadeOut(250, function() {
-                            $item.remove();
-                        });
+                        self.onItemSubmitSuccess($btn, $item, response);
                     } else {
-                        onErr();
+                        self.onItemSubmitError($btn);
                     }
                 },
                 error : function() {
-                    onErr();
+                    self.onItemSubmitError($btn);
                 },
                 complete : function() {
                     $spinner.hide();
                 }
             });
         });
+    },
+    onItemSubmitSuccess : function($btn, $item, response) {
+        $btn.text("DONE");
+        $item.delay(250).fadeOut(250, function() {
+            $item.remove();
+        });
+        this.updateItemCounts(response.item_counts);
+    },
+    onItemSubmitError : function($btn) {
+        window.alert("Sorry, but an error has occured. Please try again, and refresh the page first.");
+        $btn.prop("disabled", false);
+    },
+    updateItemCounts : function(counts) {
+        for (state in counts) {
+            var $node = $("[data-item-count-" + state.replace("STATE_", "").toLowerCase() + "]");
+            var original = $node.text();
+            $node.text(counts[state]);
+            if (original !== $node.text()) {
+                // highlight changed count
+                $node.addClass("orange work-list-highlight");
+            }
+        }
+        // clear highlights with a delay
+        setTimeout(function() { $(".work-list-highlight").removeClass("orange work-list-highlight"); }, 1000);
     }
 };
