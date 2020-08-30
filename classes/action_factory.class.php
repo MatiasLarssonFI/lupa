744807d3127b006022814adfe3c208f33f81b1be
MatiasLarssonFI <matias.larsson@gmail.com>
2020-08-30 11:55:43 +0200

    Actions factory

---
+++ classes/action_factory.class.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once(__DIR__ . "/ui_text_storage.class.php");
+
+
+/**
+ * Singleton.
+ * 
+ * Responsible for providing navigation actions.
+ * 
+ * Site-specific implementation.
+ */
+class ActionFactory {
+    private static $_inst;
+    
+    /**
+     * @return ActionFactory
+     */
+    public static function get() {
+        if (self::$_inst === null) {
+            self::$_inst = new self();
+        }
+        
+        return self::$_inst;
+    }
+    
+    
+    /**
+     * @return string[] The navigation actions
+     */
+    public function get_nav_actions() {
+        $ts = UITextStorage::get();
+        $lang = $ts->get_language();
+        $actions = [ "" ]; // front page
+    
+        if (!empty($ts->text("NEWS_CONTENT"))) {
+            $actions[] = "#news";
+        }
+        if ($lang === "fi") {
+            $actions[] = "puunkaato";
+            $actions[] = "partners";
+        }
+        $actions[] = "#contact";
+        $actions[] = "faq";
+        
+        return $actions;
+    }
+    
+    
+    protected function __construct() {}
+}
