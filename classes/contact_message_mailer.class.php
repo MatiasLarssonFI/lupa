6fd2efc7fd96be0010a6a39acc8eeb3610aad0e0
MatiasLarssonFI <matias.larsson@gmail.com>
2020-09-02 13:01:39 +0200

    Update PHPMailer

--- classes/contact_message_mailer.class.php
+++ classes/contact_message_mailer.class.php
@@ -1,20 +1,37 @@
 <?php
 
-require_once(dirname(__FILE__) . "/iemail_message.class.php");
-require_once(dirname(__FILE__) . "/imailer.class.php");
-require_once(dirname(__FILE__) . "/dbif.class.php");
-require_once(dirname(__FILE__) . "/site_config_factory.class.php");
+require_once(__DIR__ . "/iemail_message.class.php");
+require_once(__DIR__ . "/imailer.class.php");
+require_once(__DIR__ . "/dbif.class.php");
+require_once(__DIR__ . "/site_config_factory.class.php");
 
-require_once(dirname(__FILE__) . "/../lib/Twig-1.24.0/Twig-1.24.0/lib/Twig/Autoloader.php");
-require_once(dirname(__FILE__) . "/../lib/PHPMailer-5.2.21/PHPMailerAutoload.php");
+require_once(__DIR__ . "/../lib/Twig-1.24.0/Twig-1.24.0/lib/Twig/Autoloader.php");
+require_once(__DIR__ . "/../lib/PHPMailer-6.1.7/src/Exception.php");
+require_once(__DIR__ . "/../lib/PHPMailer-6.1.7/src/PHPMailer.php");
+require_once(__DIR__ . "/../lib/PHPMailer-6.1.7/src/SMTP.php");
+
+use PHPMailer\PHPMailer\PHPMailer;
 
 
 class ContactMessageMailer implements IMailer {
+    private $_mail;
+    
     public function send(IEmailMessage $contactmsg) {
+        try {
+            $this->_send($contactmsg);
+        } catch (\Exception $e) {
+            $dt = date("Y-m-d H:i:s");
+            file_put_contents(__DIR__ . "/../contact_email_error_log", "[{$dt}] {$e->getMessage()} - Mail error: {$this->_mail->ErrorInfo}" . PHP_EOL, FILE_APPEND);
+            throw $e;
+        }
+    }
+    
+    private function _send(IEmailMessage $contactmsg) {
         $mail = new PHPMailer;
+        $this->_mail = $mail;
         
         \Twig_Autoloader::register();
-        $loader = new \Twig_Loader_Filesystem(dirname(__FILE__) . "/../templates");
+        $loader = new \Twig_Loader_Filesystem(__DIR__ . "/../templates");
         $twig = new \Twig_Environment($loader, array());
         
         $host = \SiteConfigFactory::get()->get_site_config()->host();
@@ -24,13 +41,14 @@ class ContactMessageMailer implements IMailer {
         $mail->Port = 25;
         if (strlen($mail_user) > 0) { // username can also just indicate SMTP
             $mail->isSMTP();
+            $mail->Port = 587;
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Host = $db->get_mail_server();
             $mail_password = $db->get_mail_password();
             if (strlen($mail_password) > 0) {
                 $mail->SMTPAuth = true;
                 $mail->Username = $mail_user;
                 $mail->Password = $mail_password;
-                $mail->Port = 587;
             }
         }
         
@@ -42,15 +60,13 @@ class ContactMessageMailer implements IMailer {
         $mail->addAddress($db->get_contact_email());
         $mail->isHTML(true);
     
-        $mail->CharSet = 'UTF-8';
+        $mail->CharSet = PHPMailer::CHARSET_UTF8;
         $mail->Subject = "{$contactmsg->get_subject()} - LUPA contact";
         $mail->Body    = $twig->render("contact_email.html", array("message" => $contactmsg));
         $mail->AltBody = $twig->render("contact_email.txt", array("message" => $contactmsg));
 
         if(!$mail->send()) {
-            $dt = date("Y-m-d H:i:s");
-            file_put_contents(__DIR__ . "/../contact_email_error_log", "[{$dt}] {$mail->ErrorInfo}" . PHP_EOL, FILE_APPEND);
-            throw new \RuntimeException("Failed to send contact mail. Mail error: {$mail->ErrorInfo}");
+            throw new \RuntimeException("Failed to send contact form e-mail.");
         }
     }
 }
